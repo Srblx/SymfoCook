9d5d1f53faebc497267a6ffefc19271f5539a101
SXBL <122864409+Srblx@users.noreply.github.com>
2023-08-05 14:28:01 +0200

    Ajout de données en Bdd par les fixture plus quelque parametrage des fixtures (voir code

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -31,6 +31,12 @@ class Ingredient
     #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
 
+    // ^ Constructor.
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
